ca7e03a08174eb77004be7257b8c1de30f245de3
EonosLovos <enlvenlvenlv@gmail.com>
2022-11-05 23:58:40 +0000

    started porting

--- g/a/views.py
+++ g/a/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import os
+import pathlib
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,18 +8,30 @@ from django.template import loader
 
 
 def index(req):
-    the_list=[
-	{
-	'name': 'aname',
-	'id': 111
-	},
-	{
-	'name': 'bname',
-	'id': 222
-	}
-    ]
-    templ = loader.get_template('index.html')
+    default_file = "/../hamlet.txt";
+    file = default_file;
+    if req.method =='POST':
+        data = req.POST
+        post_filepath = data.get("post_filepath")
+        if(post_filepath.strip() != ""):
+            file = post_filepath
+    errorText = ""        
+    p = pathlib.Path(file)
+    if p.exists():
+        file_with_path = p.resolve()
+        if os.access(file_with_path, os.R_OK):
+            open(file_with_path, "r")#.write(my_data)
+        else:
+            errorText=f"no access to file {file}!"
+    else:
+        errorText=f"file {file} not found"
+        
+        
+    dict = []
+
     view_model = {
-	'a_list': the_list
+	'a_list': dict,
+    'file':file,
+    'errorText': errorText
     }
-    return HttpResponse(templ.render(view_model, req))
+    return render(req, 'index.html', view_model)
--- g/g/urls.py
+++ g/g/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from a import views as v
 
 urlpatterns = [
-    path('1/', include('a.urls')),
+    path('1/', v.index, name='wordCountApp'),
     path('admin/', admin.site.urls),
 ]
